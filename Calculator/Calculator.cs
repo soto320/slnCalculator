fdeb51fa15ebd986f8d4034a09745a8c0fe152b8
Saul Guaman <soto320@msn.com>
2024-06-20 16:22:02 -0500

    Windows20240620

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+        public int Multiplication(int a, int b)
+        {
+            Thread.Sleep(140);
+            return a * b;
+        }
+    }
+}
---
+++ TestCalculator/UnitTest1.cs
@@ -0,0 +1,65 @@
+using Calculator;
+using NUnit.Framework.Internal;
+
+namespace TestCalculator
+{
+    public class Tests
+    {
+        Calculator.Calculator calculator;
+        [SetUp]
+        public void Setup()
+        {
+             //this.calculator= new Calculator.Calculator();
+        }
+        [OneTimeSetUp]
+        public void OneTimeSetUP()
+        {
+            this.calculator = new Calculator.Calculator();
+        }
+
+        [Test]
+        public void Test1()
+        {
+           /* // Arrange
+            
+            int a = 5;
+            int b = 5;
+            int expectedResult = 11;
+
+            // Act
+            int actualResult = calculator.Add(a, b);
+
+            // Assert
+            //Assert.AreEqual(expectedResult, actualResult);
+            Assert.That(expectedResult, Is.EqualTo(actualResult));*/
+        }
+        [Test]
+        public void Sum_Ok() {
+            Assert.That(actual: calculator.Add(2, 4), Is.EqualTo(6));
+            System.Console.WriteLine("Test 1 Sum 2+3=6");
+        }
+        [Test]
+        public void Subtraction_Ok()
+        {
+            Assert.That(actual: calculator.Subtraction(6, 3), Is.EqualTo(3));
+            System.Console.WriteLine("Test 2 Res 6-3=3");
+        }
+        [Test]
+        public void Division_Ok()
+        {
+            Assert.That(actual: calculator.Division(6, 3), Is.EqualTo(2));
+            System.Console.WriteLine("Test 3 Div 6/3=2");
+        }
+        [Test, Timeout(150)]
+        public void Multiplication_Ok()
+        {
+            Assert.That(actual: 189, Is.EqualTo(calculator.Multiplication(6, 3)));
+            System.Console.WriteLine("Test 4 Mult 6*3=18");
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            // Cleanup code if necessary
+        }
+    }
+}
