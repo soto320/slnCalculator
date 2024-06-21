ea0989d254c79bdb921741b5266083ba4c478138
Saul Guaman <soto320@msn.com>
2024-06-21 15:37:25 -0500

    Windows20240621

--- TestCalculator/UnitTest1.cs
+++ TestCalculator/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace TestCalculator
         [SetUp]
         public void Setup()
         {
-             //this.calculator= new Calculator.Calculator();
+            //this.calculator= new Calculator.Calculator();
         }
         [OneTimeSetUp]
         public void OneTimeSetUP()
@@ -20,21 +20,22 @@ namespace TestCalculator
         [Test]
         public void Test1()
         {
-           /* // Arrange
-            
-            int a = 5;
-            int b = 5;
-            int expectedResult = 11;
+            /* // Arrange
 
-            // Act
-            int actualResult = calculator.Add(a, b);
+             int a = 5;
+             int b = 5;
+             int expectedResult = 11;
 
-            // Assert
-            //Assert.AreEqual(expectedResult, actualResult);
-            Assert.That(expectedResult, Is.EqualTo(actualResult));*/
+             // Act
+             int actualResult = calculator.Add(a, b);
+
+             // Assert
+             //Assert.AreEqual(expectedResult, actualResult);
+             Assert.That(expectedResult, Is.EqualTo(actualResult));*/
         }
         [Test]
-        public void Sum_Ok() {
+        public void Sum_Ok()
+        {
             Assert.That(actual: 6, Is.EqualTo(6));
             System.Console.WriteLine("Test 1 Sum 2+3=6");
         }
@@ -55,6 +56,68 @@ namespace TestCalculator
         {
             Assert.That(actual: 18, Is.EqualTo(calculator.Multiplication(6, 3)));
             System.Console.WriteLine("Test 4 Mult 6*3=18");
+        }
+        [Test]
+        public void ValidateAllTests()
+        {
+            List<ValidationItem> validations = new List<ValidationItem>
+        {
+            new ValidationItem
+            {
+                Expected = 5,
+                A=3,
+                B=2,
+                Action=1,
+                Message = "Test 1 Sum 2+3=6 - Passed"
+            },
+            new ValidationItem
+            {
+                Expected = 3,
+                    A=6,
+                B=3,
+                Action=2,
+                Message = "Test 2 Res 6-3=3 - Passed"
+            },
+            new ValidationItem
+            {
+                Expected = 2,
+                    A=6,
+                B=3,
+                Action=3,
+                Message = "Test 3 Div 6/3=2 - Passed"
+            },
+            new ValidationItem
+            {
+                Expected = 18,
+                    A=6,
+                B=3,
+                Action=4,
+                Message = "Test 4 Mult 6*3=18 - Passed"
+            }
+        };
+
+            foreach (var validationItem in validations)
+            {
+                Validate(validationItem);
+            }
+        }
+
+        private void Validate(ValidationItem validationItem)
+        {
+            Assert.That(validationItem.Expected, Is.EqualTo(GetActualValue(validationItem)));
+            Console.WriteLine(validationItem.Message);
+        }
+
+        private int GetActualValue(ValidationItem validationItem)
+        {
+            switch (validationItem.Action)
+            {
+                case 1: return calculator.Add(validationItem.A, validationItem.B);
+                case 2: return calculator.Subtraction(validationItem.A, validationItem.B);
+                case 3: return calculator.Division(validationItem.A, validationItem.B);
+                case 4: return calculator.Multiplication(validationItem.A, validationItem.B);
+                default: return 0;
+            }
         }
         [TearDown]
         public void TearDown()
---
+++ TestCalculator/ValidationItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCalculator
+{
+    public class ValidationItem
+    {
+        public int Expected { get; set; }
+        public int A { get; set; }
+        public int B { get; set; }
+        public int Action { get; set; }
+        public string Message { get; set; }
+    }
+}
